64ef59ab5852e8e09f10bf2efe836d01aa024508
MeleeseKaerhavn <125559675+MeleeseKaerhavn@users.noreply.github.com>
2025-08-26 08:58:51 +0200

    read input works

--- week1/guesser.js
+++ week1/guesser.js
@@ -3,6 +3,36 @@ const tilfældigeTal = Math.round(Math.random()*100);
 
 console.log("Mit tilfældige tal: " + tilfældigeTal);
 
+function readInput() {
+    return Number(document.getElementById("userGuess").value);
+}
+
+document.querySelector("#knap").addEventListener("click", compare);
+
+function compare(){
+    let gæt = readInput();
+    console.log("Gæt: " + gæt);
+    // here you can also compare gæt with your random number
+};
+
+
+
+
+// document.querySelector("#knap").addEventListener("click", readInput);
+
+
+// function readInput(){
+    
+//     let gæt = document.getElementById("userGuess").value;
+//     console.log("Gæt: " + gæt);
+//     return gæt;
+// }
+
+// console.log("Gæt: " + gæt);
+
+
+
+
 
 
 // document.querySelector(#hint).add("for højt");
